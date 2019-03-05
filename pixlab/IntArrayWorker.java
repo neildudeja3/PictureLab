ff271dfb168a5b75f7d70af2a423c49080efbc27
neildudeja3 <43892691+neildudeja3@users.noreply.github.com>
2019-03-05 12:27:22 -0800

    3/5/19 (int array worker)

--- pixlab/IntArrayWorker.java
+++ pixlab/IntArrayWorker.java
@@ -99,4 +99,19 @@ public class IntArrayWorker
     }
   }
  
+    public int getCount(int n)
+        {
+            for (int i=0 ; i<matrix.length ; i++)
+                  for (int j=0 ; j<matrix[0].length ; j++)
+                  {
+                      matrix[i]
+                    }
+                    
+                 
+            return 1;
+        }
+  
+  
 }
+
+
